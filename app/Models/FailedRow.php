3d878c70e02f3463207366d43f40570c21ea811d
RevenkoVladislav <Vlad_reven@mail.ru>
2026-01-07 19:36:43 +0600

    Стартовые модели

---
+++ app/Models/FailedRow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FailedRow extends Model
+{
+    protected $table = 'failed_rows';
+    protected $fillable = [
+
+    ];
+}
