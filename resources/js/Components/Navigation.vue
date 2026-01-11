854ba6e95b370f743565877f85f7bb75dddc4dd0
RevenkoVladislav <Vlad_reven@mail.ru>
2026-01-11 19:12:53 +0600

    Навигационное меню

---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,53 @@
+<script>
+import {Link} from '@inertiajs/vue3';
+
+export default {
+    name: "Navigation",
+
+    components: {
+        Link,
+    }
+}
+</script>
+
+<template>
+    <div class="mb-4">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-16 items-center justify-between">
+                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div class="hidden sm:ml-6 sm:block">
+                            <div class="flex space-x-4">
+                                <Link :class="['px-3 py-2 rounded-md text-sm font-medium',
+                                $page.component === 'Project/Index'
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white']"
+                                      :href="route('project.index')">
+                                    Projects
+                                </Link>
+                                <Link :class="['px-3 py-2 rounded-md text-sm font-medium',
+                                $page.component === 'Project/Import'
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white']"
+                                      :href="route('project.import')">
+                                    Import
+                                </Link>
+                                <Link :class="['px-3 py-2 rounded-md text-sm font-medium',
+                                $page.component === 'Task/Index'
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white']"
+                                      :href="route('task.index')">
+                                    Tasks
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,35 +1,27 @@
 <script>
 import { Link } from '@inertiajs/vue3';
+import Navigation from "@/Components/Navigation.vue";
 
 export default {
     name: "MainLayout",
-    components: { Link }
+    components: {
+        Link,
+        Navigation,
+    }
 }
 </script>
 
 <template>
-    <div class="w-96 mx-auto">
-        <h1>My app</h1>
+    <div class="w-full mx-auto">
+        <!-- Навигационное меню -->
+        <Navigation></Navigation>
+        <!-- Конец навигационного меню -->
 
-        <!-- Навигационные ссылки -->
-        <div
-            class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
-        >
-            <Link :href="route('dashboard')">
-                Dashboard
-            </Link>
-            <Link :href="route('project.index')">
-                Projects
-            </Link>
-            <Link :href="route('project.import')">
-                Import
-            </Link>
-            <Link :href="route('task.index')">
-                Tasks
-            </Link>
+        <!-- Вывод контента из Pages -->
+        <div class="w-4/6 mx-auto">
+            <slot />
         </div>
-
-        <slot />
+        <!-- Конец вывода контента из Pages -->
     </div>
 </template>
 
