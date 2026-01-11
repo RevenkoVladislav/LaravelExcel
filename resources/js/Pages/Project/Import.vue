e7df0277dd73ccfba1bcee1957c66cfb0fc5071e
RevenkoVladislav <Vlad_reven@mail.ru>
2026-01-11 19:36:24 +0600

    Интерфейс для импорта excel

--- resources/js/Pages/Project/Import.vue
+++ resources/js/Pages/Project/Import.vue
@@ -5,12 +5,53 @@ export default {
     name: "Import",
 
     layout: MainLayout,
+
+    data() {
+        return {
+            excelFile: null,
+        }
+    },
+
+    methods: {
+
+        /**
+         * Загрузка файла при клике на кнопку
+         */
+        selectExcel() {
+            this.$refs.file.click();
+        },
+
+        /**
+         * Запишем в data загруженный файл
+         */
+        setExcel(file) {
+            this.excelFile = file.target.files[0];
+        },
+
+        /**
+         *
+         */
+        importExcel() {
+            console.log(111);
+        },
+    }
 }
 </script>
 
 <template>
     <div>
-        Import
+        <h1 class="mb-3">Import</h1>
+
+        <div class="flex">
+            <form>
+                <input @change="setExcel" type="file" ref="file" class="hidden">
+                <button @click.prevent="selectExcel" class="block rounded-full w-32 text-center text-white p-2 bg-gradient-to-r from-green-500 to-green-600 hover:bg-gradient-to-bl">Excel</button>
+            </form>
+            <div v-if="excelFile" class="ml-3">
+                <button @click.prevent="importExcel" class="block rounded-full w-32 text-center text-white p-2 bg-gradient-to-r from-sky-500 to-sky-600 hover:bg-gradient-to-bl">Import</button>
+            </div>
+        </div>
+
     </div>
 </template>
 
