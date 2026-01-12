fc937786bd4ba611bef99ce1d2ac878997012403
RevenkoVladislav <Vlad_reven@mail.ru>
2026-01-12 21:34:13 +0600

    Импорт excel файла в бд

--- app/Imports/ExcelImport.php
+++ app/Imports/ExcelImport.php
@@ -2,17 +2,81 @@
 
 namespace App\Imports;
 
+use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ExcelImport implements ToCollection
+class ExcelImport implements ToCollection, WithHeadingRow
 {
+    /**
+     * Получаем названия из таблицы Types
+     *
+     * Проходимся по строкам в загруженном файле
+     * Если у нас пустая строка - наименование, то продолжаем проходить по файлу
+     * Создаем запись в бд прокинув туда все данные из файла
+     */
 
     public function collection(Collection $collection)
     {
+        $typesMap = $this->getTypesMap(Type::all());
+
         foreach ($collection as $row) {
-            dd($row);
+            if (!isset($row['naimenovanie'])) continue;
+
+            Project::create([
+                'type_id' => $this->getTypeId($typesMap, $row['tip']),
+                'title' => $row['naimenovanie'],
+                'creation_date' => Date::excelToDateTimeObject($row['data_sozdaniia']),
+                'contracted_date' => Date::excelToDateTimeObject($row['podpisanie_dogovora']),
+                'deadline' => isset($row['dedlain']) ? Date::excelToDateTimeObject($row['dedlain']) : null,
+                'is_chain' => isset($row['setevik']) ? $this->getBool($row['setevik']) : null,
+                'is_on_time' => isset($row['sdaca_v_srok']) ? $this->getBool($row['sdaca_v_srok']) : null,
+                'has_outsource' => isset($row['nalicie_autsorsinga']) ? $this->getBool($row['nalicie_autsorsinga']) : null,
+                'has_investors' => isset($row['nalicie_investorov']) ? $this->getBool($row['nalicie_investorov']) : null,
+                'workers_count' => $row['kolicestvo_ucastnikov'] ?? null,
+                'services_count' => $row['kolicestvo_uslug'] ?? null,
+                'payment_first_step' => $row['vlozenie_v_pervyi_etap'] ?? null,
+                'payment_second_step' => $row['vlozenie_vo_vtoroi_etap'] ?? null,
+                'payment_third_step' => $row['vlozenie_v_tretii_etap'] ?? null,
+                'payment_fourth_step' => $row['vlozenie_v_cetvertyi_etap'] ?? null,
+                'comment' => $row['kommentarii'] ?? null,
+                'efficiency_value' => $row['znacenie_effektivnosti'] ?? null,
+            ]);
         }
+    }
+
+    /**
+     * Получаем массив данных из таблицы Types
+     * Где ключ - title из таблицы Types, а значение id из таблицы Types
+     */
+    private function getTypesMap($types): array
+    {
+        $map = [];
+        foreach ($types as $type) {
+            $map[$type->title] = $type->id;
+        }
+
+        return $map;
+    }
+
+    /**
+     * Проверяем, есть ли тип в таблице Types
+     * Если есть - вернем его
+     * Если нет создадим и запросим его id
+     */
+    private function getTypeId($map, $title)
+    {
+        return isset($map[$title]) ? $map[$title] : Type::create(['title' => $title])->id;
+    }
+
+    /**
+     * Вспомогательный метод, для преобразования из excel файла 'да/нет' в 'true/false'
+     */
+    private function getBool($item): bool
+    {
+        return $item === "Да" ? true : false;
     }
 }
